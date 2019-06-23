d96034bbe6ff80f0f22ecb80041d793b909726ec
xiaopy <990402387@qq.com>
2019-06-23 16:42:14 +0800

    1 引入liquibase 做数据库管理工具

---
+++ src/main/java/com/xiaopy/springboot/demo/demo/CompletableFutureDemo.java
@@ -0,0 +1,12 @@
+package com.xiaopy.springboot.demo.demo;
+
+/**
+ * @author Xiaopy
+ * @since  2019/6/11
+ */
+public class CompletableFutureDemo {
+
+    public static void main(String[] args) {
+
+    }
+}
